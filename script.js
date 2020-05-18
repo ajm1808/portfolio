f1dfc2b26b2d5afb6d30de4d4631abc2c229047d
ajm1808 <ajm180885@gmail.com>
2020-05-18 14:19:17 +0100

    added github links

--- script.js
+++ script.js
@@ -1,16 +1,7 @@
 const menu=document.querySelector(".menu-toggle-btn");
 const table= document.querySelector(".tablemenubar");
 
-menu.addEventListener("click",()=>{
-    if(table.style.display=="none"){
-        table.style.display="block";
-        menu.textContent="show"
-    }
-    else {
-        image.style.display="none";
-        button.textContent="show"
-    }
-})
+
 
 const aboutBTN=document.querySelector(".about-btn");
 const modalBg=document.querySelector(".modal-bg");
