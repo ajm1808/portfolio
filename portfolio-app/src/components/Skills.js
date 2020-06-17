f06e087105ccc8b7f0d4ed5e18a69cbc6e9b39ff
ajm1808 <ajm180885@gmail.com>
2020-06-17 16:36:25 +0100

    added framer motion to skills table

--- portfolio-app/src/components/Skills.js
+++ portfolio-app/src/components/Skills.js
@@ -13,60 +13,57 @@ const Skills =()=>{
         <div className="container3">
             <div className="skillsTable">
                 <div className="toprow">
-                    <a id="lang">
                         Language
-                    </a>
-                    <a id="scale">
-                        My skills on a scale of 0 to Mad...
-                    </a>
                 </div>
                 <div className="html">
-                    <a id="Icon">
-                        <img id="Icon" src={html}/>
-                    </a>
-                    <div className="score">
-                        <div className="skills html">40%</div>
-                    </div>
+
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={html}/>
                 </div>
                 <div className="css">
                     <a id="Icon">
-                        <img id="Icon" src={css}/>
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={css}/>
                     </a>
-                    <div className="score">
-                        <div className="skills css">50%</div>
-                    </div>
                 </div>
                 <div className="js">
                     <a id="Icon">
-                        <img id="Icon" src={java}/>
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={java}/>
                     </a>
-                    <div className="score">
-                        <div className="skills js">30%</div>
-                    </div>
                 </div>
                 <div className="photoshop">
                     <a id="Icon">
-                        <img id="Icon" src={photo}/>
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={photo}/>
                     </a>
-                    <div className="score">
-                        <div className="skills photo">45%</div>
-                    </div>
                 </div>
                 <div className="react">
                     <a id="Icon">
-                        <img id="Icon" src={react}/>
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={react}/>
                     </a>
-                    <div className="score">
-                        <div className="skills react">30%</div>
-                    </div>
                 </div>
                 <div className="node">
                     <a id="Icon">
-                        <img id="Icon" src={node}/>
+                        <motion.img whileHover={{ scale: 1.2, rotate: 360 }} id="Icon" src={node}/>
                     </a>
-                    <div className="score">
-                        <div className="skills node">20%</div>
-                    </div>
+                </div>
+                <div className="toprow">
+                        On a rating of 1 to Mad...
+                </div>
+                <div className="score scorehtml">
+                   <a id="s">40%</a>
+                </div>
+                <div className="score scorecss">
+                       <a id="s">50%</a>
+                </div>
+                <div className="score scorejs">
+                       <a id="s">30%</a>
+                </div>
+                <div className="score scorereact">
+                        <a id="s">30%</a>
+                </div>
+                <div className="score scorephoto">
+                        <div id="s">45%</div>
+                </div>
+                <div className="score scorenode">
+                        <a id="s">20%</a>
                 </div>
             </div>
         </div>
